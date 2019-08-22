b7ae0c3f89489e138a55be33b8c3444b349f18c1
mavstronaut <mavbarona@gmail.com>
2019-08-22 13:13:02 -0400

    Updates

---
+++ precache-manifest.c67268f409eb6258d06dc246c0cc6534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd910be1fb73f47a3bb135ed6f3a6b3",
+    "url": "/clicky-react-game/index.html"
+  },
+  {
+    "revision": "3f381dfe3f732b531af4",
+    "url": "/clicky-react-game/static/css/main.32b4d9dd.chunk.css"
+  },
+  {
+    "revision": "e081d685ba5708eef6a3",
+    "url": "/clicky-react-game/static/js/2.e2ea5766.chunk.js"
+  },
+  {
+    "revision": "3f381dfe3f732b531af4",
+    "url": "/clicky-react-game/static/js/main.407f8609.chunk.js"
+  },
+  {
+    "revision": "a421bf9e03885dae9a34",
+    "url": "/clicky-react-game/static/js/runtime~main.a3bc75db.js"
+  }
+]);
